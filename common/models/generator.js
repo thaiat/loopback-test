8b5fe9076d8c8b1a473f8a669b5dc5f1318c9898
Joshua Estrin Skrzypek <jskrzypek@gmail.com>
2016-03-03 19:35:55 +0200

    feat(app): Add createView and executeQuery methods to generator

--- common/models/generator.js
+++ common/models/generator.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = function(Generator) {
     Generator.createModel = function(config, cb) {
         var modelName = config.name;
@@ -12,7 +14,7 @@ module.exports = function(Generator) {
 
         var ds = Generator.app.datasources[datasourceName];
 
-        var Model = ds.createModel(modelName, properties, config); 
+        var Model = ds.createModel(modelName, properties, config);
         Generator.app.model(Model);
 
         ds.automigrate(Model.modelName, function(err) {
@@ -27,7 +29,11 @@ module.exports = function(Generator) {
 
     Generator.remoteMethod(
         'createModel', {
-            accepts: { arg: 'config', type: 'object' },
+            accepts: {
+                arg: 'config',
+                type: 'object',
+                description: 'Config should contain a modelName, ds, properties, and any other model creation options'
+            },
             returns: {
                 arg: 'result',
                 type: 'string',
@@ -38,4 +44,84 @@ module.exports = function(Generator) {
             }
         }
     );
+
+    Generator.createView = function(config, cb) {
+        var datasourceName = config.ds || 'db';
+        delete config.ds;
+        var params = config.params || [];
+        delete config.params;
+        var viewName = config.viewName;
+        delete config.viewName;
+        var query = config.query
+        delete config.query;
+
+        var ds = Generator.app.datasources[datasourceName];
+
+        if (_.isFunction(ds.connector.executeSQL)) {   
+            var dropQuery = 'DROP VIEW ' + viewName;
+            var createQuery = 'CREATE VIEW ' + viewName + ' AS ' + query + ';';         
+            ds.connector.executeSQL(dropQuery, params, {}, function(err, result) {
+                ds.connector.executeSQL(createQuery, params, {}, cb);
+            });
+        } else {
+            cb(new Error('DataSource ' + datasourceName + '\'s connector does not have an executeSQL method for executing this view.'))
+        }
+
+    }
+
+    Generator.remoteMethod(
+        'createView', {
+            accepts: {
+                arg: 'config',
+                type: 'object',
+                description: 'Config should contain a viewName, ds, query, params'
+            },
+            returns: {
+                arg: 'result',
+                type: 'string',
+                root: true
+            },
+            http: {
+                verb: 'post'
+            }
+        }
+    );
+
+    Generator.executeQuery = function(config, cb) {
+        var datasourceName = config.ds || 'db';
+        delete config.ds;
+        var params = config.params || [];
+        delete config.params;
+        var query = config.query
+        delete config.query;
+
+        var ds = Generator.app.datasources[datasourceName];
+
+        if (_.isFunction(ds.connector.executeSQL)) {   
+            var View = ds.connector.executeSQL(query, params, {}, cb);
+        } else {
+            cb(new Error('DataSource ' + datasourceName + '\'s connector does not have an executeSQL method for executing this view.'))
+        }
+
+    }
+
+    Generator.remoteMethod(
+        'executeQuery', {
+            accepts: {
+                arg: 'config',
+                type: 'object',
+                description: 'Config should contain a ds, query, params'
+            },
+            returns: {
+                arg: 'result',
+                type: 'string',
+                root: true
+            },
+            http: {
+                verb: 'post'
+            }
+        }
+    );
+
+
 };
